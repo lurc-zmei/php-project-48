b99ac70c0715a95c1335fdf9c58c6551d61ebd9a
lurc-zmei <lurc-zmei@inbox.ru>
2025-11-06 10:13:26 +0300

    Add genDiff static func, at the screen

--- src/Parser.php
+++ src/Parser.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Hexlet\Code;
+
+use function Funct\Collection\sortBy;
+
 class Parser
 {
 //    private $filePath;
@@ -9,16 +12,59 @@ class Parser
 //    {
 //        $this->filePath = $filePath;
 //    }
-    public function parse($filePath)
+    public function parse($filePath) : array
     {
         if (file_exists($filePath)) {
             $absolutePath = realpath($filePath);
             $content = file_get_contents($absolutePath);
-            $data = json_decode($content);
+            $data = json_decode($content, true);
         } else {
             $data = 'File not found';
         }
         return $data;
+    }
+
+    public static function genDiff($filePath1, $filePath2) : string
+    {
+        $parser = new Parser;
+        $data1 = $parser->parse($filePath1);
+        $data2 = $parser->parse($filePath2);
+        $allKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
+
+        $sortedKeys = sortBy($allKeys, function ($key) {
+            return $key;
+        });
+
+        $result = [];
+
+        foreach ($sortedKeys as $key) {
+            if (isset($data1[$key]) && isset($data2[$key])) {
+                if ($data1[$key] === $data2[$key]) {
+                    $result[] = "  $key: {$parser->formatValue($data1[$key])}";
+                } else {
+                    $result[] = "- $key: {$parser->formatValue($data1[$key])}";
+                    $result[] = "+ $key: {$parser->formatValue($data2[$key])}";
+                }
+            }
+            if (isset($data1[$key]) && !isset($data2[$key])) {
+                $result[] = "- $key: {$parser->formatValue($data1[$key])}";
+            }
+            if (!isset($data1[$key]) && isset($data2[$key])) {
+                $result[] = "+ $key: {$parser->formatValue($data2[$key])}";
+            }
+        }
+        return "{\n" . implode("\n", $result) . "\n}";
+    }
+
+    function formatValue($value)
+    {
+        if (is_bool($value)) {
+            return $value ? 'true' : 'false';
+        }
+        if (is_null($value)) {
+            return 'null';
+        }
+        return $value;
     }
 
 }
